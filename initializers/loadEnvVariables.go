ce803f57bdf10c3ca7ec79ab2c3d12ee9620e602
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2023-08-23 18:23:26 +0700

    first commit

---
+++ initializers/loadEnvVariables.go
@@ -0,0 +1,14 @@
+package initializers
+
+import (
+	"log"
+	"github.com/joho/godotenv"
+)
+
+// LoadEnvVariables loads the .env file into the environment variables
+func LoadEnvVariables() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
