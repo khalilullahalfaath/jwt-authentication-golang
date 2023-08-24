fe386bd7645a5a04371872d4aa34a8c7d1a72f54
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2023-08-24 10:16:30 +0700

    create login function

--- controllers/usersController.go
+++ controllers/usersController.go
@@ -2,8 +2,11 @@ package controllers
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/khalilullahalfaath/jwt-authentication-golang/initializers"
 	"github.com/khalilullahalfaath/jwt-authentication-golang/models"
 	"golang.org/x/crypto/bcrypt"
@@ -40,5 +43,55 @@ func SignUp(c *gin.Context) {
 
 	// respond
 	c.JSON(http.StatusOK, gin.H{"message": "User created successfully"})
+}
 
+func Login(c *gin.Context) {
+	// get the email and password from the request body
+	var body struct {
+		Email    string
+		Password string
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Parameters can't be empty"})
+		return
+	}
+
+	// find the user
+	var user models.User
+	result := initializers.DB.Where("email = ?", body.Email).First(&user)
+
+	if result.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	// compare the password
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	// generate the token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while generating the token"})
+		return
+	}
+
+	// respons as a cookie
+	c.SetSameSite(http.SameSiteNoneMode)
+	c.SetCookie("token", tokenString, 3600, "", "", false, true)
+
+
+	// // respond
+	// c.JSON(http.StatusOK, gin.H{"token": tokenString})
 }
